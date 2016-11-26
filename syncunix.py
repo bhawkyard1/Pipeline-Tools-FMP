bc286667063cdadcf3107624d3df4b95d0ba46bf
Ben Hawkyard <i7620560@bournemouth.ac.uk>
2016-11-26 12:52:08 +0000

    Gdrive is really annoying, going to try a slightly different approach, culling files client side.

--- syncunix.py
+++ syncunix.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import shutil
 import glob
 import util
 
@@ -14,22 +15,43 @@ def pull():
 
 def push():
 	print "Pushing project to online repo..."
+	
 	for root, dirs, files in os.walk( glob.globs["PROJECT_ROOT"] + "/production" ):
 		files = []
 		for dir in dirs:
-			rp = os.path.relpath( dir, glob.globs["PROJECT_ROOT"] )
-			print "checking " + root + ", " + rp + ", " + dir + " " + util.getConfigValue( glob.assetProductionPath( dir ), "CHECKEDOUT" )
+			rp = os.path.relpath( glob.assetProductionPath( dir ), glob.globs["PROJECT_ROOT"] )
+			ap = os.path.join( root, dir )
 			if util.getConfigValue( glob.assetProductionPath( dir ), "CHECKEDOUT" ) == "True":
-				pass
-				#upload( rp, rp )
+				upload( ap, rp )
 				
 	print "done"
 	#cmd = "~/gdrive upload " + glob.globs["PROJECT_ROOT"] + '/ -r'# + " --path " + glob.globs["ONLINE_ROOT"]
 	#print cmd
 	#util.log( glob.globs["PROJECT_ROOT"], "Project pushed." )
 	#subprocess.Popen( [cmd], shell = True ).wait(
+
+def gdrive_GetID( _path ):
+	line = subprocess.check_output( ["~/gdrive list | grep " + _path], shell = True )
+	return line.split(' ')[0]
+	
+def gdrive_MakeDir( _path ):
+	folders = _path.split("/")
+	curID = ""
+	for f in folders:
+		curID = subprocess.check_output( ["~/gdrive mkdir " + f], shell = True )
+		curID = curID.split(' ')[1]
+	
+def upload( _absPath, _remotePath ):
+	print "Uploading " + _absPath + " to " + _remotePath
+	
+	parentID = ""
+	try:
+		parentID = gdrive_GetID( _remotePath )		
+	except:
+		print "Path does not exist! Creating folders..."
+		subprocess.Popen( ["~/gdrive mkdir" + _remotePath], shell = True ).wait()
+		parentID = gdrive_GetID( _remotePath )
 		
-def upload( _localPath, _remotePath ):
-	print "Uploading " + _localPath + " to " + _remotePath
-	cmd = "~/gdrive upload " + _localPath + '/ -r' + " --p " + _remotePath
+	cmd = "~/gdrive upload " + _absPath + '/ -r' + " --p " + parentID
+		
 	subprocess.Popen( [cmd], shell = True ).wait()
